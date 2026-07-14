8d2899f30a562b01974823c1faf9b09c5b30114a
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-14 18:00:09 +0530

    es modules

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // require('./batman');
 // require('./superman');
 
-const SuperHero = require('./super-hero');
-const batman = new SuperHero('Batman')
-console.log(batman.getName());
-batman.setName('Bruce Wayne');
-console.log(batman.getName());
+// const SuperHero = require('./super-hero');
+// const batman = new SuperHero('Batman')
+// console.log(batman.getName());
+// batman.setName('Bruce Wayne');
+// console.log(batman.getName());
 
-const superman = new SuperHero('Superman');
-console.log(superman.getName());
+// const superman = new SuperHero('Superman');
+// console.log(superman.getName());
+
+const math = require('./math');
+console.log(math.add(2,3));
+console.log(math.subtract(2,3))
---
+++ main.mjs
@@ -0,0 +1,4 @@
+import math from "./math-esm.mjs";
+
+console.log(math.add(8,3));
+console.log(math.subtract(8,3))
