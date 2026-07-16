e4391e43070582707a5412595bc94f6088562b92
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-16 22:51:19 +0530

    eventemitter

--- app.js
+++ app.js
@@ -10,11 +10,11 @@
 // const superman = new SuperHero('Superman');
 // console.log(superman.getName());
 
-const math = require('./math');
-console.log(math.add(2,3));
-console.log(math.subtract(2,3));
+// const math = require('./math');
+// console.log(math.add(2,3));
+// console.log(math.subtract(2,3));
 
-const path = require('path');
+// const path = require('node:path');
 // console.log(__filename);
 // console.log(__dirname);
 
@@ -35,9 +35,39 @@ const path = require('path');
 // console.log(path.join("\\folder1","", "//images", "poster.jpg"));
 // console.log(path.join(__dirname, "poster.jpg"));
 
-console.log(path.resolve("movies", "images", "poster.jpg"));
+/* console.log(path.resolve("movies", "images", "poster.jpg"));
 console.log(path.resolve("\\folder1","folder2", "images", "poster.jpg"));
 console.log(path.resolve("folder1","", "images", "poster.jpg"));
 console.log(path.resolve("folder1","", "images", "poster.jpg"));
-console.log(path.resolve(__dirname, "poster.jpg"));
+console.log(path.resolve(__dirname, "poster.jpg")); */
 
+const callback = (name) => {
+    console.log(`Hello ${name}`);
+}
+
+const higherOrderFuction = (callback) => {
+    const name='Jilu';
+    callback(name);
+}
+
+higherOrderFuction(callback);
+
+
+// const EventEmitter = require('node:events');
+// const emitter = new EventEmitter();
+
+// emitter.on("order-pizza", (size, toppings) => {
+//     console.log(`Order Received for ${size} size pizza with ${toppings} toppings `)
+// })
+
+// emitter.emit("order-pizza","large", "mushroom");
+
+const PizzaShop = require('./pizza-shop');
+const pizzaShop = new PizzaShop();
+
+pizzaShop.on("order",(size, toppings) => {
+    console.log(`Order Received!!! Baking a ${size} pizza with ${toppings} toppings`)
+})
+
+pizzaShop.order("large", "mushroom");
+pizzaShop.displayOrderNumber();
