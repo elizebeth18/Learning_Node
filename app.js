8f6aa903f80b675ee19f977aa12ffef674e67190
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-16 12:56:53 +0530

    path the built-in modules and the related utilities

--- app.js
+++ app.js
@@ -12,4 +12,20 @@
 
 const math = require('./math');
 console.log(math.add(2,3));
-console.log(math.subtract(2,3))
+console.log(math.subtract(2,3));
+
+const path = require('path');
+console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
+console.log("Format ======> "+path.format(path.parse(__filename)));
+
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("./data.json"))
+
