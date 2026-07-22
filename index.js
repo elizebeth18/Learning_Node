cbf15f738d8ab16fdc1974083c5d07c4943b1545
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-22 21:20:11 +0530

    create http-server and respond with plain text

---
+++ index.js
@@ -0,0 +1,13 @@
+//import http module
+const http = require('node:http');
+
+//create server
+const server = http.createServer((req, res) => {
+    console.log("req=========>",req);
+    res.writeHead(200, { "Content-Type": "text/plain"});
+    res.end("Hello World!!!")
+});
+
+server.listen(3000, ()=> {
+    console.log('Server running on port 3000')
+});
