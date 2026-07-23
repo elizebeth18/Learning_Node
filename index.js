703a286409826026534433fe921aabb29e2c5007
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-23 11:06:29 +0530

    send JSON response from the server

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const http = require('node:http');
 
 //create server
 const server = http.createServer((req, res) => {
-    console.log("req=========>",req);
-    res.writeHead(200, { "Content-Type": "text/plain"});
-    res.end("Hello World!!!")
+    console.log("req=========>", req);
+
+    const superHero = {
+        firstName: "Bruce",
+        lastName: "Wayne",
+    }
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(superHero))
 });
 
-server.listen(3000, ()=> {
+server.listen(3000, () => {
     console.log('Server running on port 3000')
 });
